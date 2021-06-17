ecc2817eebb67cacbbd4aae3fc517b8f3ce591ce
Chris Morrow <cmorrow@redventures.com>
2021-06-17 17:15:46 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <input type="text" v-model="nameInput.firstName" placeholder="first name" />
+  <input type="text" v-model="nameInput.lastName" placeholder="last name" />
+  <button @click="changeUser">Change User</button>
+
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Counter />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Counter from "./components/Counter.vue";
+import { provide, reactive, readonly } from "vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    Counter
+  },
+  setup() {
+    const user = reactive({
+      id: 1,
+      firstName: "Chris",
+      lastName: "Morrow"
+    });
+    const nameInput = reactive({
+      id: 1,
+      firstName: user.firstName,
+      lastName: user.lastName
+    });
+
+    console.log(nameInput);
+
+    provide("currentUser", readonly(user));
+
+    const changeUser = function() {
+      user.firstName = nameInput.firstName;
+      user.lastName = nameInput.lastName;
+    };
+
+    return {
+      user,
+      nameInput,
+      changeUser
+    };
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- Counter -->
+  <div>
+    <button @click="subtract">-</button>
+    <button @click="add">+</button>
+    <div class="counter">{{ counter }}</div>
+    <h2>Current User:</h2>
+    <p>{{ currentUser.firstName }} {{ currentUser.lastName }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, inject } from "vue";
+
+export default {
+  name: "Counter",
+  setup() {
+    const counter = ref(0);
+
+    const currentUser = inject("currentUser");
+    currentUser.firstName = "Bob";
+
+    const add = function() {
+      counter.value = counter.value + 1;
+    };
+
+    const subtract = function() {
+      counter.value = counter.value - 1;
+    };
+
+    return {
+      currentUser,
+      add,
+      subtract,
+      counter
+    };
+  }
+};
+</script>
+
+<style scoped>
+button {
+  margin-left: 5px;
+}
+</style>
